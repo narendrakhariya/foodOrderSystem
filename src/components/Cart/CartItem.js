2bf8f94f00007bf53543926cafc065857498887b
Narendra Khariya <khariyanaresh007@gmail.com>
2023-02-18 09:18:50 +0900

    Add and remove item to cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -6,13 +6,12 @@ const CartItem = (props) => {
   return (
     <li className={classes["cart-item"]}>
       <div>
-        <h4>{props.name}</h4>
+        <h2>{props.name}</h2>
         <div className={classes.summary}>
           <span className={classes.price}>{price}</span>
           <span className={classes.amount}>x {props.amount}</span>
         </div>
       </div>
-      \ |
       <div className={classes.actions}>
         <button onClick={props.onRemove}>−</button>
         <button onClick={props.onAdd}>+</button>
